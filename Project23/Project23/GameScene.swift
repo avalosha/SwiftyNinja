78b98a6d53f74def2bc2c9a833d2b0ca5cac7d82
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-10-05 22:29:50 -0500

    Draw lines with CGPath and UIBezierPath.

--- Project23/Project23/GameScene.swift
+++ Project23/Project23/GameScene.swift
@@ -24,6 +24,10 @@ class GameScene: SKScene {
     var activeSliceBG: SKShapeNode!
     var activeSliceFG: SKShapeNode!
     
+    var activeSlicePoints = [CGPoint]()
+    
+    var isSwooshSoundActive = false
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "sliceBackground")
         background.position = CGPoint(x: 512, y: 384)
@@ -75,4 +79,83 @@ class GameScene: SKScene {
         addChild(activeSliceBG)
         addChild(activeSliceFG)
     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        activeSlicePoints.append(location)
+        redrawActiveSlice()
+        if !isSwooshSoundActive {
+            playSwooshSound()
+        }
+    }
+    
+    //Desaparece el efecto al dejar de tocar la pantalla
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        activeSliceBG.run(SKAction.fadeOut(withDuration: 0.25))
+        activeSliceFG.run(SKAction.fadeOut(withDuration: 0.25))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+
+        // Remueve todos los puntos existentes
+        activeSlicePoints.removeAll(keepingCapacity: true)
+
+        // Agrega la ubicaciòn tactil
+        let location = touch.location(in: self)
+        activeSlicePoints.append(location)
+
+        // Metodo para limpiar las formas deslizables
+        redrawActiveSlice()
+
+        // Remueve cualquier acción sobre el efecto
+        activeSliceBG.removeAllActions()
+        activeSliceFG.removeAllActions()
+
+        // Alpha de 1
+        activeSliceBG.alpha = 1
+        activeSliceFG.alpha = 1
+    }
+    
+    func redrawActiveSlice() {
+        // Sí tiene menos de 2 puntos no se dibuja la linea
+        if activeSlicePoints.count < 2 {
+            activeSliceBG.path = nil
+            activeSliceFG.path = nil
+            return
+        }
+
+        // Sí tiene más de 12 puntos elimina los más antiguos
+        if activeSlicePoints.count > 12 {
+            activeSlicePoints.removeFirst(activeSlicePoints.count - 12)
+        }
+
+        // Comienza a dibujar la linea con el arreglo de puntos
+        let path = UIBezierPath()
+        path.move(to: activeSlicePoints[0])
+
+        for i in 1 ..< activeSlicePoints.count {
+            path.addLine(to: activeSlicePoints[i])
+        }
+
+        // Dibuja la línea siguiendo la ruta
+        activeSliceBG.path = path.cgPath
+        activeSliceFG.path = path.cgPath
+    }
+    
+    func playSwooshSound() {
+        isSwooshSoundActive = true
+
+        let randomNumber = Int.random(in: 1...3)
+        let soundName = "swoosh\(randomNumber).caf"
+
+        let swooshSound = SKAction.playSoundFileNamed(soundName, waitForCompletion: true)
+
+        //El completion closure se llamara hasta terminar la música
+        run(swooshSound) { [weak self] in
+            self?.isSwooshSoundActive = false
+        }
+    }
+    
 }
